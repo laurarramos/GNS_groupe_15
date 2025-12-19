f461e96d06df9709c5464992985475c87d896b7b
Maksim <maksim_neples@hotmail.com>
2025-12-19 17:40:18 +0100

    version1 à corriger

--- automatisation/config_auto.py
+++ automatisation/config_auto.py
@@ -2,15 +2,11 @@ import json
 import re
 import time
 import telnetlib
-from ipaddress import IPv6Network
 import sys
 
 from gns3fy import Gns3Connector, Project, Node
 
 GNS3_URL = "http://127.0.0.1:3080"         
-DEFAULT_OSPF_AREA = 0
-OSPF_PROCESS_ID = 1
-TELNET_DELAY = 0.3
 
 with open("intent.json", "r", encoding="utf-8") as f:
     intent = json.load(f)
@@ -20,6 +16,25 @@ project = Project(name="structure_vide", connector=server)
 project.get()      
 project.open()     
 project.get_nodes()
+
+def reset_router(node):
+    tn = telnetlib.Telnet(node.console_host, node.console)
+    time.sleep(1)
+    tn.write(b"\r\n")
+    time.sleep(0.3)
+    tn.write(b"enable\r\n")
+    time.sleep(0.3)
+    tn.write(b"write erase\r\n")
+    time.sleep(0.3)
+    tn.write(b"\r\n")
+    time.sleep(0.5)
+    tn.write(b"reload\r\n")
+    time.sleep(0.3)
+    tn.write(b"\r\n")
+    time.sleep(0.5)
+
+    tn.write(b"no\r\n")  # ne pas sauvegarder
+    tn.close()
 
 def config_RIP(node,router_name,as_routeur):
     #ouverture écriture dans console :
@@ -34,16 +49,18 @@ def config_RIP(node,router_name,as_routeur):
     time.sleep(0.3)
     tn.write(b"ipv6 unicast-routing\r\n")
     time.sleep(0.3)
-    tn.write(b"ipv6 router rip {intent[AS][as_routeur].get('nom_process')}\r\n")
+    nom_process = intent["AS"][as_routeur].get("nom_process")
+    tn.write(f"ipv6 router rip {nom_process}\r\n".encode("ascii"))
     time.sleep(0.3)
     #loopback :
     tn.write(b"interface Loopback0\r\n")
     time.sleep(0.3)
     tn.write(b"ipv6 enable\r\n")
     time.sleep(0.3)
-    tn.write(b"ipv6 address {intent[routeurs][router_name].get('loopback')}\r\n")
+    loopback_address = intent["routeurs"][router_name].get("loopback")
+    tn.write(f"ipv6 address {loopback_address}\r\n".encode("ascii"))
     time.sleep(0.3)
-    tn.write(b"ipv6 rip {intent[AS][as_routeur].get('nom_process')} enable\r\n")
+    tn.write(f"ipv6 rip {nom_process} enable\r\n".encode("ascii"))
     time.sleep(0.3)
     tn.write(b"exit\r\n")
     time.sleep(0.3)
@@ -51,14 +68,14 @@ def config_RIP(node,router_name,as_routeur):
     for link in intent["links"]:
         if router_name == link["routeur_a"]:
             voisin = link["routeur_b"]
-            tn.write(b"interface {link[interface_a]}\r\n")
+            tn.write(f"interface {link["interface_a"]}\r\n".encode("ascii"))
             time.sleep(0.3)
             tn.write(b"ipv6 enable\r\n")
             time.sleep(0.3)
-            tn.write(b"ipv6 address {link[sous_res]}\r\n")
+            tn.write(f"ipv6 address {link["sous_res"]}\r\n".encode("ascii"))
             time.sleep(0.3)
             if intent["routeurs"][voisin].get("as") == as_routeur:
-                tn.write(b"ipv6 rip {intent[AS][as_routeur].get('nom_process')} enable\r\n")
+                tn.write(f"ipv6 rip {nom_process} enable\r\n".encode("ascii"))
                 time.sleep(0.3)
             tn.write(b"no shutdown\r\n")
             time.sleep(0.3)
@@ -66,21 +83,22 @@ def config_RIP(node,router_name,as_routeur):
             time.sleep(0.3)
         elif router_name == link["routeur_b"]:
             voisin = link["routeur_a"]
-            tn.write(b"interface {link[interface_b]}\r\n")
+            tn.write(f"interface {link["interface_b"]}\r\n".encode("ascii"))
             time.sleep(0.3)
             tn.write(b"ipv6 enable\r\n")
             time.sleep(0.3)
-            tn.write(b"ipv6 address {link[sous_res]}\r\n")
+            tn.write(f"ipv6 address {link["sous_res"]}\r\n".encode("ascii"))
             time.sleep(0.3)
             if intent["routeurs"][voisin].get("as") == as_routeur:
-                tn.write(b"ipv6 rip {intent[AS][as_routeur].get('nom_process')} enable\r\n")
+                tn.write(f"ipv6 rip {nom_process} enable\r\n".encode("ascii"))
                 time.sleep(0.3)
             tn.write(b"no shutdown\r\n")
             time.sleep(0.3)
             tn.write(b"exit\r\n")
             time.sleep(0.3)
     tn.write(b"end\r\n")
-    tn.write(b"write\r\n")
+    time.sleep(1)
+    tn.write(b"write memory\r\n")
     tn.close()
 
 def config_OSPF(node,router_name,as_routeur):
@@ -90,18 +108,17 @@ def config_OSPF(node,router_name,as_routeur):
     tn.write(b"\r\n")
     time.sleep(0.5)
     #Début des commandes :
-    tn.write(b"enable\r\n")
+    tn.write(f"enable\r\n".encode("ascii"))
     time.sleep(0.3)
-    tn.write(b"configure terminal\r\n")
+    tn.write(f"configure terminal\r\n".encode("ascii"))
     time.sleep(0.3)
-    tn.write(b"ipv6 unicast-routing\r\n")
-    time.sleep(0.3)
-    tn.write(b"ipv6 unicast-routing\r\n")
+    tn.write(f"ipv6 unicast-routing\r\n".encode("ascii"))
     time.sleep(0.3)
     #routeur ID :
-    tn.write(b"ipv6 router ospf 1\r\n") #1 variable ?
+    tn.write(f"ipv6 router ospf 1\r\n".encode("ascii")) #1 variable ?
     time.sleep(0.3)
-    tn.write(b"router id {intent[routeurs][router_name].get('routeurID')}\r\n")
+    routeurID = intent["routeurs"][router_name].get("routeurID")
+    tn.write(f"router-id {routeurID}\r\n".encode("ascii"))
     time.sleep(0.3)
     tn.write(b"exit\r\n")
     time.sleep(0.3)
@@ -110,7 +127,8 @@ def config_OSPF(node,router_name,as_routeur):
     time.sleep(0.3)
     tn.write(b"ipv6 enable\r\n")
     time.sleep(0.3)
-    tn.write(b"ipv6 address {intent[routeurs][router_name].get('loopback')}\r\n")
+    loopback_address = intent["routeurs"][router_name].get("loopback")
+    tn.write(f"ipv6 address {loopback_address}\r\n".encode("ascii"))
     time.sleep(0.3)
     tn.write(b"ipv6 ospf 1 area 0\r\n") #voir si 1 et 0 peuvent être variables
     time.sleep(0.3)
@@ -120,38 +138,50 @@ def config_OSPF(node,router_name,as_routeur):
     for link in intent["links"]:
         if router_name == link["routeur_a"]:
             voisin = link["routeur_b"]
-            tn.write(b"interface {link[interface_a]}\r\n")
+            tn.write(f"interface {link["interface_a"]}\r\n".encode("ascii"))
             time.sleep(0.3)
             tn.write(b"ipv6 enable\r\n")
             time.sleep(0.3)
-            tn.write(b"ipv6 address {link[sous_res]}\r\n")
+            tn.write(f"ipv6 address {link["sous_res"]}\r\n".encode("ascii"))
             time.sleep(0.3)
             if intent["routeurs"][voisin].get("as") == as_routeur:
                 tn.write(b"ipv6 ospf 1 area 0\r\n")
                 time.sleep(0.3)
+            tn.write(b"no shutdown\r\n")
+            time.sleep(0.3)
+            tn.write(b"exit\r\n")
+            time.sleep(0.3)
         elif router_name == link["routeur_b"]:
             voisin = link["routeur_a"]
-            tn.write(b"interface {link[interface_b]}\r\n")
+            tn.write(f"interface {link["interface_b"]}\r\n".encode("ascii"))
             time.sleep(0.3)
             tn.write(b"ipv6 enable\r\n")
             time.sleep(0.3)
-            tn.write(b"ipv6 address {link[sous_res]}\r\n")
+            tn.write(f"ipv6 address {link["sous_res"]}\r\n".encode("ascii"))
             time.sleep(0.3)
             if intent["routeurs"][voisin].get("as") == as_routeur:
                 tn.write(b"ipv6 ospf 1 area 0\r\n")
                 time.sleep(0.3)
-        tn.write(b"no shutdown\r\n")
-        time.sleep(0.3)
-        tn.write(b"exit\r\n")
-        time.sleep(0.3)
+            tn.write(b"no shutdown\r\n")
+            time.sleep(0.3)
+            tn.write(b"exit\r\n")
+            time.sleep(0.3)
     tn.write(b"end\r\n")
-    tn.write(b"write\r\n")
+    time.sleep(1)
+    tn.write(b"write memory\r\n")
     tn.close()
 
 def main():
     for router_name, router_info in intent.get("routeurs", {}).items():
         node = Node(project_id=project.project_id, name=router_name, connector=server)
         node.get()
+        #renitialisation du routeur
+        #reset_router(node)
+
+        if node.status != "started":
+            node.start()
+            time.sleep(2)  
+            node.get()
         #info générales du routeur
         as_routeur = intent["routeurs"][router_name].get("as")
         protocole_routeur = intent["AS"][as_routeur].get("igp")
@@ -162,4 +192,6 @@ def main():
         elif protocole_routeur == "OSPF":
             config_OSPF(node, router_name, as_routeur)
 
-    
+
+if __name__ == "__main__":
+    main()
